c02b0a84d597d15e5878b556219f5f12f423518d
Mayank Thakur <mamayankt.2002@gmail.com>
2021-04-12 10:25:07 +0300

    Allows looping over multiple files, and control final width of image

--- main.py
+++ main.py
@@ -1,37 +1,56 @@
 import cv2
 import numpy as np
 from os import system
-import multiprocessing
 
-cap = cv2.VideoCapture("D:\Documents\Code\Python\movieImage\movie.mp4")
-width_center = int((cap.get(cv2.CAP_PROP_FRAME_WIDTH)/2))
-height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
-img = np.zeros((int(height),1,3),np.uint8)
-frame_count = 0
+def fps_cont(frame_count, width):
+    if width == 1:
+        return 1
+    else:
+        return int(frame_count/width)
 
-try:
-    total_frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
-except:
-    total_frames = 1
-    print("Frames percentage is broken, unable to get total frame count")
+def make_barcode(movie_path, image_path, width_pass):
 
-while(1):
-    ret, frame = cap.read()
+    cap = cv2.VideoCapture(movie_path)
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    img = np.zeros((int(height),1,3),np.uint8)
+    frame_count = 0
 
-    frame_show = frame[:, width_center:width_center+1] #width and then height limitation
+    try:
+        total_frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+    except:
+        total_frames = 1
+        print("Frames percentage is broken, unable to get total frame count")
 
-    if frame_count % int(total_frames/2500) == 0:
-        img = cv2.hconcat([img, frame_show])
-        print(str(float(frame_count/total_frames*100))+" % Done!")
+    while(1):
+        ret, frame = cap.read()
 
-    cv2.imshow('frame',frame_show)
+        if cv2.waitKey(1) & 0xFF == ord('q') or ret==False :
+            cap.release()
+            cv2.destroyAllWindows()
+            break
 
-    if cv2.waitKey(1) & 0xFF == ord('q') or ret==False :
-        cap.release()
-        cv2.destroyAllWindows()
+        if frame_count % fps_cont(total_frames, width_pass) == 0:
+            img = cv2.hconcat([img, cv2.resize(frame, (1,height))])
+            if int(frame_count/total_frames*100) != int((frame_count-1)/total_frames*100):
+                print(str(int(frame_count/total_frames*100))+" % Done!")
+
+        frame_count += 1
+        cv2.imshow('frame',cv2.resize(img, (width, height)))
+
+    cv2.imwrite(image_path, img)
+    print("Image created!")
+
+for i in range(2,8):
+    movie_path = "D:\Documents\Code\Python\movieImage\movie" + str(i) + ".mp4"
+    pic_path = "D:\Documents\Code\Python\movieImage\pic" + str(i) + ".png"
+
+    img_rand = np.ones((100,100,3),np.uint8)
+    cv2.imwrite(pic_path, img_rand)
+
+    cont = make_barcode(movie_path, pic_path, 4500)
+
+    if cont == False:
         break
 
-    frame_count += 1
-
-cv2.imwrite("D:\Documents\Code\Python\movieImage\pic.png", img)
-print("Image created!")
+print("Done!")
